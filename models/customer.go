5e85aa69d5ac5458d3327f302822db2fdd664b2f
Manu Gupta <manugupt1@gmail.com>
2018-08-11 13:19:15 -0400

    Refactor validate function

--- models/customer.go
+++ models/customer.go
@@ -6,12 +6,11 @@ import (
 
 	"github.com/gobuffalo/validate"
 	"github.com/gobuffalo/validate/validators"
-
+	"github.com/pkg/errors"
 	"golang.org/x/crypto/bcrypt"
 
 	"github.com/gobuffalo/pop"
 	"github.com/gobuffalo/uuid"
-	"github.com/pkg/errors"
 )
 
 type Customer struct {
@@ -41,14 +40,12 @@ func (c Customers) String() string {
 // Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
 // It hashes the password using bcrypt before storing in the database
 func (c *Customer) Validate(tx *pop.Connection) (*validate.Errors, error) {
-	validationErrors := validate.Validate(
+	return validate.Validate(
 		&validators.StringIsPresent{Name: "Password", Field: c.Password},
 		&validators.StringIsPresent{Name: "Name", Field: c.Name},
 		&validators.EmailIsPresent{Name: "Email", Field: c.Email, Message: "Email is not in the right format"},
-	)
-	if validationErrors.Count() > 0 {
-		return validationErrors, nil
-	}
+	), nil
+}
 
 	hash, err := bcrypt.GenerateFromPassword([]byte(c.Password), bcrypt.DefaultCost)
 	if err != nil {
