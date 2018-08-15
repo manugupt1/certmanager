00096633f9f0362003c6be284a086aacb20968dd
Manu Gupta <manugupt1@gmail.com>
2018-08-14 22:20:52 -0400

    Change layout of certificate page

---
+++ assets/js/certificate.js
@@ -0,0 +1,27 @@
+import {Get, Post, Patch} from './requests.js';
+import {updateStatus} from './status.js';
+
+$('#certificates').ready(() => {
+  get_active_cert();
+});
+
+function get_active_cert() {
+  console.log("get active cert");
+}
+
+function create_cert() {
+
+}
+
+function download_key(id) {
+
+}
+
+function download_cert(id) {
+  
+}
+
+export function deactivate_cert() {
+
+}
+
