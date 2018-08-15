ed6fe90386b0f691d6b3a07d359e1058c8243e08
Manu Gupta <manugupt1@gmail.com>
2018-08-15 19:53:45 -0400

    Add ability to deactivate a certificate

--- assets/js/certificate.js
+++ assets/js/certificate.js
@@ -2,15 +2,17 @@ import {Get, Post, Patch} from './requests.js';
 import {updateStatus} from './status.js';
 
 $('#certificates').ready(() => {
+  // debugger;
   get_active_cert();
 });
+
 
 function get_active_cert() {
   Get("/certificate/list"+window.location.search)
   .then((response) => {
     response.json()
       .then((data) => {
-        let certRows = document.getElementById("#certificate_rows");
+        let certRows = document.getElementById("certificate_rows");
         if (certRows) {
           certRows.innerText = "";
         }
@@ -19,13 +21,14 @@ function get_active_cert() {
             + "<td>" + row.id + "</td>"
             + "<td>" + row.key_path + "</td>"
             + "<td>" + row.body_path + "</td>"
-            + "<td><a>Deactivate</a></td>"
+            + "<td id='"+row.id+"'><a href='#'>Deactivate</a></td>"
             + "</tr>";
-          $("#certificate_rows").prepend(el)
+          $("#certificate_rows").prepend(el);
+          var elObj = document.getElementById(row.id);
+          elObj.addEventListener('click', deactivate_cert.bind({cert_id: row.id}));
         }
       });
   })
-  console.log("get active cert");
 }
 
 function create_cert() {
@@ -37,10 +40,22 @@ function download_key(id) {
 }
 
 function download_cert(id) {
-  
+
 }
 
 export function deactivate_cert() {
-
+  const url = "/certificate/" + this.cert_id + "/update?active=false";
+  Patch(url)
+    .then((response) => {
+      const status = response.status;
+      if (status == 200) {
+        updateStatus("Certificate deactivated")
+        get_active_cert();
+      } else {
+        response.json()
+          .then((data) => {
+            updateStatus(data)
+          })
+      }
+    })
 }
-
--- assets/js/requests.js
+++ assets/js/requests.js
@@ -51,3 +51,21 @@ export const Get = (url = ``) => {
           referrer: "no-referrer", // no-referrer, *client
       })
   };
+
+
+    // From MDN Documentation
+export const Patch = (url = ``) => {
+    // Default options are marked with *
+      return fetch(url, {
+          method: "PATCH", // *GET, POST, PUT, DELETE, etc.
+          mode: "cors", // no-cors, cors, *same-origin
+          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: "same-origin", // include, same-origin, *omit
+          headers: {
+              "Content-Type": "application/json; charset=utf-8",
+              // "Content-Type": "application/x-www-form-urlencoded",
+          },
+          redirect: "follow", // manual, *follow, error
+          referrer: "no-referrer", // no-referrer, *client
+      })
+  };
